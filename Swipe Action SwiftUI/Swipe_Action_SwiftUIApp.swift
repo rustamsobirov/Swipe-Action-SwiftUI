1d6ca7b779db0be79acdf0253755eeac2b504619
admin <carwolt123@gmail.com>
2023-03-06 14:09:13 +0500

    Initial Commit

---
+++ Swipe Action SwiftUI/Swipe_Action_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_Action_SwiftUIApp.swift
+//  Swipe Action SwiftUI
+//
+//  Created by iMac G1 on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_Action_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
